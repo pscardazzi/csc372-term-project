4550e86efadfe50416594f1249ce3afa6cb370e2
pscardazzi <p_magalhaes@uncg.edu>
2026-05-05 19:22:17 -0400

    api check

--- react-frontend/components/LoginComponent.jsx
+++ react-frontend/components/LoginComponent.jsx
@@ -7,7 +7,7 @@ function LoginComponent({ onLogin }) {
   async function handleLogin(e) {
   e.preventDefault();
 
-  const res = await fetch("http://localhost:3000/api/users/login", {
+  const res = await fetch("api/users/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
