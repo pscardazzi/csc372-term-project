08868f19dd592a9725216e01c4d6ef895d9daae7
pscardazzi <p_magalhaes@uncg.edu>
2026-05-05 19:16:16 -0400

    server check

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,20 @@ app.use(express.static("public"));
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server listening on port: " + PORT + "!");
